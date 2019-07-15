12374d463736562db289d0401014f66a926cc5af
Full Name <nickfuentes24@gmail.com>
2019-07-15 12:18:15 -0500

    Wrote a javscript program to find out if your word is a palindrome or not. Prints message to console.

--- Javascript-Excersises/js/palindrome.js
+++ Javascript-Excersises/js/palindrome.js
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+function reverseString(word) {
+    let revword = "";
+    for (let index = word.length - 1; index >= 0; index--) {
+        revword = revword + word[index]
+    }
+    if (word == revword) {
+        console.log(`Your Word: [${word}] Is A Palindrome!`)
+    }
+    else {
+        console.log(`Your Word: [${word}] Is NOT Palindrome!`)
+    }
+}
+
+console.log(reverseString("nickolas"))

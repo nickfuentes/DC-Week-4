d0113fc1c5a1f9857a4bdf7c5020c553d6fe68d5
Full Name <nickfuentes24@gmail.com>
2019-07-15 12:33:44 -0500

    Created a javascript function that takes a number and prints a message if its even or odd to the console.

---
+++ Javascript-Excersises/js/odd-even.js
@@ -0,0 +1,10 @@
+function odd_even(num) {
+    if (num % 2 == 0) {
+        console.log(`Your Number: [${num}] is EVEN!`)
+    }
+    else {
+        console.log(`Your Number: [${num}] is ODD!`)
+    }
+}
+
+odd_even(11)

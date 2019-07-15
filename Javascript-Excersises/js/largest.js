f5425a7e06e9f46c4702062f3aada0932355849f
Full Name <nickfuentes24@gmail.com>
2019-07-15 13:13:03 -0500

    Wrote a function that loops throuhgh an array and finds the biggest number and prints t out to the terminal.

---
+++ Javascript-Excersises/js/largest.js
@@ -0,0 +1,13 @@
+let array = [6, 3, 21, 89, 10, 32, 9]
+
+function largest() {
+    largest_num = 0
+    for (let index = 0; index < array.length; index++) {
+        if (largest_num < array[index]) {
+            largest_num = array[index]
+        }
+    }
+    console.log(largest_num)
+}
+
+largest()

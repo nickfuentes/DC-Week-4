ee0cfcf019f11c2c6d4693bfe5d4d8eaccdbcc01
Full Name <nickfuentes24@gmail.com>
2019-07-15 13:43:44 -0500

    Changed the starting index number to be 1 since its allready the smalles number value.

--- Javascript-Excersises/js/largest.js
+++ Javascript-Excersises/js/largest.js
@@ -2,7 +2,7 @@ let array = [6, 3, 21, 89, 10, 90, 32, 9]
 
 function largest() {
     largest_num = array[0]
-    for (let index = 0; index < array.length; index++) {
+    for (let index = 1; index < array.length; index++) {
         if (largest_num < array[index]) {
             largest_num = array[index]
         }

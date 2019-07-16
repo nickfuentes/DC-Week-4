485852ddc4cae9f874a5f98ec078d12ddf0215f1
Full Name <nickfuentes24@gmail.com>
2019-07-16 11:25:05 -0500

    Wrote some jQuery to when you refresh the page the image will be random.

--- good-ads/app.js
+++ good-ads/app.js
@@ -1,6 +1,7 @@
 
 // call the getRandomAd function
-
+let randPhoto = getRandomAd()
 // get the adImage by id
-
-// set the src property to the new random ad image  
+let adIMAGE = document.getElementById("adImage")
+// set the src property to the new random ad image
+adIMAGE.src = randPhoto

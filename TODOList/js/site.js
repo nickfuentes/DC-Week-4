def05b790baf943cbb629abe12f479a309339193
Full Name <nickfuentes24@gmail.com>
2019-07-16 18:43:25 -0500

    Created a the remove link to delete the todo and created a check function that if checked moves to completed taks if not stays inside pending tasks.

--- TODOList/js/site.js
+++ TODOList/js/site.js
@@ -2,36 +2,54 @@ let addButton = document.getElementById("addButton")
 let todoTextBox = document.getElementById("todoTextBox")
 
 let pendingTasksDiv = document.getElementById("pendingTasks")
+let completedTasks = document.getElementById("completedTasks")
 
+// Adds div to DOM, checkbox, h3, a into div 
 addButton.addEventListener("click", function () {
-    console.log("Button is being fired....")
+
     // grabs the value of text box
     let todo = todoTextBox.value
 
     // creates a div
     let pendingTasksDIV = document.createElement("div")
     pendingTasksDIV.className = "tasksDiv"
-
     // creates a checkbox
     let checkbox = document.createElement("input")
+    checkbox.className = "checkbox"
     checkbox.type = "checkbox"
-
-    // creates a p
-    let todoParagraph = document.createElement("p")
+    checkbox.addEventListener("click", moveList)
+    // creates a h3
+    let todoParagraph = document.createElement("h3")
     todoParagraph.innerHTML = todo
     todoParagraph.className = "tasksParagraph"
-
     //creates button
     let todoLinkRemove = document.createElement("a")
-    todoLinkRemove.className = "linkStyle"
+    todoLinkRemove.className = "link"
     todoLinkRemove.text = "Remove"
+    todoLinkRemove.addEventListener("click", removeTodo)
 
+    // grabs the div pendingTasks and adds a div to it
+    pendingTasksDiv.appendChild(pendingTasksDIV)
     // puts checkbox inside the div
     pendingTasksDIV.appendChild(checkbox)
     // puts the paragraph element into the pendingTaks div
     pendingTasksDIV.appendChild(todoParagraph)
+    // adds remove link to div
     pendingTasksDIV.appendChild(todoLinkRemove)
-    // grabs the div pendingTasks and adds a div to it
-    pendingTasksDiv.appendChild(pendingTasksDIV)
 
 })
+
+// removes item grabs the parent element of the link remove and removes it
+function removeTodo() {
+    this.parentElement.parentElement.removeChild
+        (this.parentElement)
+}
+
+// completes the task and moves to other list
+function moveList() {
+    if (this.checked) {
+        completedTasks.appendChild(this.parentElement)
+    } else {
+        pendingTasks.appendChild(this.parentElement)
+    }
+}

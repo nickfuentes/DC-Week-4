57c88e28790e01b02fc2f3035b3fddf4229eaa75
Full Name <nickfuentes24@gmail.com>
2019-07-16 15:01:51 -0500

    Wrote a todo list that you can add items to a lit with jquery creating elements and pushing to the DOM.

--- TODOList/js/site.js
+++ TODOList/js/site.js
@@ -1,1 +1,37 @@
-console.log("Hello Nick")
+let addButton = document.getElementById("addButton")
+let todoTextBox = document.getElementById("todoTextBox")
+
+let pendingTasksDiv = document.getElementById("pendingTasks")
+
+addButton.addEventListener("click", function () {
+    console.log("Button is being fired....")
+    // grabs the value of text box
+    let todo = todoTextBox.value
+
+    // creates a div
+    let pendingTasksDIV = document.createElement("div")
+    pendingTasksDIV.className = "tasksDiv"
+
+    // creates a checkbox
+    let checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
+
+    // creates a p
+    let todoParagraph = document.createElement("p")
+    todoParagraph.innerHTML = todo
+    todoParagraph.className = "tasksParagraph"
+
+    //creates button
+    let todoLinkRemove = document.createElement("a")
+    todoLinkRemove.className = "linkStyle"
+    todoLinkRemove.text = "Remove"
+
+    // puts checkbox inside the div
+    pendingTasksDIV.appendChild(checkbox)
+    // puts the paragraph element into the pendingTaks div
+    pendingTasksDIV.appendChild(todoParagraph)
+    pendingTasksDIV.appendChild(todoLinkRemove)
+    // grabs the div pendingTasks and adds a div to it
+    pendingTasksDiv.appendChild(pendingTasksDIV)
+
+})

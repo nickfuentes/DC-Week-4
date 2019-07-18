a3e0e22f1a9450fc99a70be2fb672331304defd6
Full Name <nickfuentes24@gmail.com>
2019-07-18 13:52:04 -0500

    Wrote a program in Javascript that will pull the quote from the quotes object and update the user every 3 seconds.

---
+++ Stock-Quotes/js/main.js
@@ -0,0 +1,35 @@
+let textSymbolBox = document.getElementById("textSymbolBox")
+let quoteListDiv = document.getElementById("mainDiv")
+let searchButton = document.getElementById("searchButton")
+
+
+
+
+searchButton.addEventListener("click", function () {
+
+    let symbol = textSymbolBox.value
+
+    // Prints the Quote to the screen when clicked
+    prinToScreen()
+
+    // Gets the quote and puts it into the html
+    function prinToScreen() {
+        let chosenQuote = getStockQuote(symbol)
+
+        let insertQuote =
+            `
+                            <div>
+                                <p>${chosenQuote.name}</p>
+                                <p>${chosenQuote.price}</p>
+                            </di>
+                            `
+        quoteListDiv.innerHTML = insertQuote
+    }
+
+    // Set Interval Timer to insert into HTML every 3 seconds.
+    let timer = setInterval(prinToScreen, 3000);
+
+})
+
+
+

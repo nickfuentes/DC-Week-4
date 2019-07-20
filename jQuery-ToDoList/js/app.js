7403630758a8498d007cbf54a989b67a06207ab8
Full Name <nickfuentes24@gmail.com>
2019-07-19 23:16:08 -0500

    Created div and appened to the DOM.

--- jQuery-ToDoList/js/app.js
+++ jQuery-ToDoList/js/app.js
@@ -6,4 +6,28 @@ $(document).ready(function () {
     let pendingTodosDiv = $("#pendingTodosDiv")
     let completedTodosDiv = $("#completedTodosDiv")
 
+    addButton.click(function () {
+
+        // stores the value of the in the input box
+        let todo = todoTextBox.val()
+
+        // creates a div
+        let pendingTodoList = $("<div>")
+        pendingTodoList.addClass("pendingTodos")
+
+        // creates checkbox
+        let checkbox = $("<input/>").attr({ type: "checkbox" })
+        checkbox.addClass("checkbox")
+
+
+        // append checkbox into todoDiv
+        pendingTodoList.append(checkbox)
+        // append todoTitle into todoDiv
+        pendingTodoList.append(todoTitle)
+
+        // append todoDiv into mainDiv
+        pendingTodosDiv.append(pendingTodoList)
+
+    })
+
 })

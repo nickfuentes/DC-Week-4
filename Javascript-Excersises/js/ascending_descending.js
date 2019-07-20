4e573885b8b353d3312b0dc5411ed20b94a8cf3c
Full Name <nickfuentes24@gmail.com>
2019-07-19 23:53:19 -0500

    Working on coding challenges in Javascript.

---
+++ Javascript-Excersises/js/ascending_descending.js
@@ -0,0 +1,10 @@
+let array = [3, 4, 56, 7, 8, 1]
+
+function ascending() {
+    for (let index = 0; index < array.length; index++) {
+        console.log(array[index])
+
+    }
+}
+
+ascending()
--- Javascript-Excersises/js/remove-dups.js
+++ Javascript-Excersises/js/remove-dups.js
@@ -1,1 +1,26 @@
-console.log("Hello Remove!")
+
+let names = ["Alex", "Mary", "John", "Mary", "Alex", "Jerry"]
+let duplicateFreeArray = []
+
+function doesNameExists(name) {
+    let alreadyExists = false
+    for (let index = 0; index < duplicateFreeArray.length; index++) {
+        if (name == duplicateFreeArray[index]) {
+            alreadyExists = true
+            break
+        }
+    }
+    return alreadyExists
+}
+
+for (let index = 0; index < names.length; index++) {
+    let name = names[index]
+    // if the name does not exists in duplicateFreeArray
+    if (!doesNameExists(name)) {
+        duplicateFreeArray.push(name)
+    }
+}
+
+console.log(duplicateFreeArray)
+
+//console.log(!false)

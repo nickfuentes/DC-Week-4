6df92bac476b9d1035e4f1b8f7398498557a75d8
Full Name <nickfuentes24@gmail.com>
2019-07-19 23:55:52 -0500

    Recreating the TODO List with template literals.

---
+++ Template-Literal-TODOList/js/site.js
@@ -0,0 +1,12 @@
+let addButton = document.getElementById("addButton")
+let todoTextBox = document.getElementById("todoTextBox")
+let pendingTasksDiv = document.getElementById("pendingTasks")
+
+addButton.addEventListener("click", () => {
+
+    let todoTitle = todoTextBox.nodeValue
+
+    letTaskItemDiv = 
+
+})
+

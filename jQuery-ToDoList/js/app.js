647fee72e2c6c9e73575c728a915ac8908ca0a49
Full Name <nickfuentes24@gmail.com>
2019-07-19 23:19:40 -0500

    Created created the title h3 and remove tag and appeneded it to the DOM.

--- jQuery-ToDoList/js/app.js
+++ jQuery-ToDoList/js/app.js
@@ -10,20 +10,25 @@ $(document).ready(function () {
 
         // stores the value of the in the input box
         let todo = todoTextBox.val()
-
         // creates a div
         let pendingTodoList = $("<div>")
         pendingTodoList.addClass("pendingTodos")
-
         // creates checkbox
         let checkbox = $("<input/>").attr({ type: "checkbox" })
         checkbox.addClass("checkbox")
-
+        // creat a h3
+        let todoTitle = $("<h3>")
+        todoTitle.html(todo)
+        // creates a tag
+        let remove = $("<a>")
+        remove.html("Remove")
 
         // append checkbox into todoDiv
         pendingTodoList.append(checkbox)
         // append todoTitle into todoDiv
         pendingTodoList.append(todoTitle)
+        // append remove into todoDiv
+        pendingTodoList.append(remove)
 
         // append todoDiv into mainDiv
         pendingTodosDiv.append(pendingTodoList)

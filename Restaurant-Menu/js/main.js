d1b59035dc13a1b32798703224f2bbcc976a870f
Full Name <nickfuentes24@gmail.com>
2019-07-20 22:07:02 -0500

    Refactored the code so one function maps or filters the code with a conditional. And another function writes a template literal and inserts it into the DOM.

--- Restaurant-Menu/js/main.js
+++ Restaurant-Menu/js/main.js
@@ -7,137 +7,74 @@ let starterButton = document.getElementById("fullStartersButton")
 let entreeButton = document.getElementById("fullEntreesButton")
 let dessertButton = document.getElementById("fullDessertsButton")
 
-// adds full list of menu on page
-let dishesDiv = dishes.map(dish => {
+let getDishes = (course) => {
 
-    let dishDiv =
+    if (course === "Full Menu") {
+        return dishes
+    } else {
+        return dishes.filter((dish) => dish.course === course)
+    }
+}
 
-        `<div class="courseDiv">
-            <div class="courseImg">
-                <img src="${dish.imageURL}" />
-            </div>
-            <div class="courseTitleDescrip">
-                <h2>${dish.title}</h2>
-                <p>${dish.description}</p>
-            </div>
-            <div class="coursePrice">
-                <p>${dish.price}</p>
-            </div>
-        </div>`
-    return dishDiv
+let writeCourseToDOM = (course) => {
 
-})
+    let displayDishes = getDishes(course)
 
-let fullMenuTitle = `<h1>Full Menu</h1>`
 
-courseTitle.innerHTML = fullMenuTitle
-dishesListDiv.innerHTML = dishesDiv.join("")
+    let mapDishes = displayDishes.map(dish => {
+
+        let dishDiv =
+
+            `<div class="courseDiv">
+                        <div class="courseImg">
+                            <img src="${dish.imageURL}" />
+                        </div>
+                        <div class="courseTitleDescrip">
+                            <h2>${dish.title}</h2>
+                            <p>${dish.description}</p>
+                        </div>
+                        <div class="coursePrice">
+                            <p>${dish.price}</p>
+                        </div>
+                    </div>`
+        return dishDiv
+
+    })
+
+    // Update the course title 
+    courseTitle.innerHTML = course
+    // Write courses down
+    dishesListDiv.innerHTML = mapDishes.join("")
+}
+
+writeCourseToDOM("Full Menu")
+
+
 
 // (Full Button) 
 fullButton.addEventListener("click", function () {
-    let fullMenuTitle = `<h1>Full Menu</h1>`
 
-    courseTitle.innerHTML = fullMenuTitle
-    dishesListDiv.innerHTML = dishesDiv.join("")
+    writeCourseToDOM("Full Menu")
+
 })
 
 // (Starter Button)
 starterButton.addEventListener("click", function () {
 
-    let filteredStarters = dishes.filter(function (starters) {
-        return starters.course === "Starters"
-    })
-
-    // map through all the starters
-    let starters = filteredStarters.map(dish => {
-
-        let dishDiv =
-
-            `<div class="courseDiv">
-                <div class="courseImg">
-                    <img src="${dish.imageURL}" />
-                </div>
-                <div class="courseTitleDescrip">
-                    <h2>${dish.title}</h2>
-                    <p>${dish.description}</p>
-                </div>
-                <div class="coursePrice">
-                    <p>${dish.price}</p>
-                </div>
-            </div>`
-        return dishDiv
-
-    })
-    let fullMenuTitle = `<h1>Starters</h1>`
-
-    courseTitle.innerHTML = fullMenuTitle
-    // enters the template litereal into the html
-    dishesListDiv.innerHTML = starters.join("")
+    writeCourseToDOM("Starters")
 
 })
 
 // (Entree Button)
 entreeButton.addEventListener("click", function () {
 
-    let filteredEntrees = dishes.filter(function (entrees) {
-        return entrees.course === "Entrees"
-    })
+    writeCourseToDOM("Entrees")
 
-    // map through all the starters
-    let entrees = filteredEntrees.map(dish => {
-
-        let dishDiv =
-
-            `<div class="courseDiv">
-            <div class="courseImg">
-                <img src="${dish.imageURL}" />
-            </div>
-            <div class="courseTitleDescrip">
-                <h2>${dish.title}</h2>
-                <p>${dish.description}</p>
-            </div>
-            <div class="coursePrice">
-                <p>${dish.price}</p>
-            </div>
-        </div>`
-        return dishDiv
-
-    })
-    let fullMenuTitle = `<h1>Entrees</h1>`
-
-    courseTitle.innerHTML = fullMenuTitle
-    dishesListDiv.innerHTML = entrees.join("")
 })
 
 // (Dessert Button)
 dessertButton.addEventListener("click", function () {
 
-    let filteredDesserts = dishes.filter(function (desserts) {
-        return desserts.course === "Desserts"
-    })
-
-    let desserts = filteredDesserts.map(dish => {
-
-        let dishDiv =
-
-            `<div class="courseDiv">
-                <div class="courseImg">
-                    <img src="${dish.imageURL}" />
-                </div>
-                <div class="courseTitleDescrip">
-                    <h2>${dish.title}</h2>
-                    <p>${dish.description}</p>
-                </div>
-                <div class="coursePrice">
-                    <p>${dish.price}</p>
-                </div>
-            </div>`
-        return dishDiv
-
-    })
-    let fullMenuTitle = `<h1>Desserts</h1>`
-
-    courseTitle.innerHTML = fullMenuTitle
-    dishesListDiv.innerHTML = desserts.join("")
+    writeCourseToDOM("Desserts")
 
 })
